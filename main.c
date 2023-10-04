19fdd2eb021f26a9cb11af82bb263d52ff4aa356
reality_user <jc3124567@gmail.com>
2023-10-04 16:15:28 -0500

    put subexrPtrs in a Expr ** and alloc'd a Expr* for each one which makes things a lot simpler

--- main.c
+++ main.c
@@ -111,15 +111,35 @@ Expr *makeExpr(ExprType etype, size_t subexpr_count, void *subexprs) {
   return exprPtr;
 }
 
-void freeSubexpr(Expr *exprPtr) {
-  switch (exprPtr->etype) {
-  case ValueExpr: 
-    freeValue(exprPtr->subexprs);
-    break;
-  case BinAddExpr:
-    assert(0 && "TODO");
-  }
+void *makeValueExprAt(Expr *exprPtr, Value *valuePtr) {
+  makeExprAt(exprPtr, ValueExpr, 1, valuePtr);
 }
+
+Expr *makeValueExpr(Value *valuePtr) {
+  Expr *exprPtr = exprAlloc();
+  makeValueExprAt(exprPtr, valuePtr);
+  return exprPtr;
+}
+
+Expr *makeBinAddExpr(Expr *subexpr0, Expr *subexpr1) {
+  Expr **subexprs = malloc(2 * sizeof (Expr *));
+  
+  // makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
+  // makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
+  subexprs[0] = subexpr0;
+  subexprs[1] = subexpr1;
+  return makeExpr(BinAddExpr, 2, subexprs);
+}
+
+// void freeSubexpr(Expr *exprPtr) {
+//   switch (exprPtr->etype) {
+//   case ValueExpr: 
+//     freeValue(exprPtr->subexprs);
+//     break;
+//   case BinAddExpr:
+//     assert(0 && "TODO");
+//   }
+// }
 
 /*
  * recursively free expression, subexprs, and values in subexprs.
@@ -136,9 +156,10 @@ void freeExpr(Expr *exprPtr) {
     printf("freeing bin add expression\n");
     for (int i = 0; i < exprPtr->subexpr_count; ++i) {
       printf("freeing bin add subexpr %d\n", i);
-      freeSubexpr((Expr *)(exprPtr->subexprs) + i);
+      //freeSubexpr((Expr *)(exprPtr->subexprs) + i);
+      freeExpr(((Expr **)exprPtr->subexprs)[i]);
     }
-    printf("freeing subexpression dynamic array");
+    printf("freeing subexpression dynamic array\n");
     free(exprPtr->subexprs);
     break;
   default:
@@ -157,6 +178,7 @@ void freeValueExpr(Expr *exprPtr) {
 
 void breaker() { }
 
+
 int main(int argc, char **argv) {
   
   Value *value1 = make_ui64Value(12345);
@@ -172,10 +194,24 @@ int main(int argc, char **argv) {
   // makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
   // Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
 
-  Expr *expr2_subexprs = malloc(2 * sizeof (Expr));
-  makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
-  makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
-  Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
+
+  // // THIS USED TO WORK BUT I BROKE IT INTENTIONALLY
+  // Expr *expr2_subexprs = malloc(2 * sizeof (Expr));
+  // makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
+  // makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
+  // Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
+
+  Expr *expr2 = makeBinAddExpr(
+    makeValueExpr(make_ui64Value(700)),
+    makeValueExpr(make_ui64Value(77)));
+
+  Expr *expr3 = makeBinAddExpr(
+    makeBinAddExpr(
+      makeValueExpr(make_ui64Value(1000)),
+      makeValueExpr(make_ui64Value(100))),
+    makeBinAddExpr(
+      makeValueExpr(make_ui64Value(10)),
+      makeValueExpr(make_ui64Value(1))));
 
   breaker();
 
@@ -203,6 +239,10 @@ int main(int argc, char **argv) {
 
   printf("free expr2\n");
   freeExpr(expr2);
+  printf("\n");
+
+  printf("free expr3\n");
+  freeExpr(expr3);
   printf("\n");
   
   return 0;
