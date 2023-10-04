2bc0776b0229eaae7e605ae3ab900317bd849663
reality_user <jc3124567@gmail.com>
2023-10-04 15:06:29 -0500

    added freeSubexpr function

--- Makefile
+++ Makefile
@@ -1,2 +1,7 @@
 main: main.c
 	gcc -o main main.c
+
+debug: main.c
+	gcc -g -o debug main.c
+
+
--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <string.h>
 #include <stdbool.h>
+#include <assert.h>
 
 typedef enum {
   ui64Value,
@@ -41,6 +42,9 @@ struct Expr {
  * vtype -- type of value to make
  * size -- number of octabytes to allocate for *octabytes
  * *values -- ptr to values to set octabytes to
+ * 
+ * must be passed a valid vtype and valid size. *values is optional and may be
+ * NULL.
  */
 Value *makeValue(ValueType vtype, size_t size, void *values) {
   Value *value = malloc(sizeof (Value));
@@ -68,10 +72,14 @@ Value *makeValue(ValueType vtype, size_t size, void *values) {
   return value;
 }
 
+void make_ui64ValueAt(Value* valuePtr, uint64_t x) {
+  valuePtr->octabytes[0] = x;
+}
+
 Value *make_ui64Value(uint64_t x) {
-  Value *value = makeValue(ui64Value, 1, NULL);
-  value->octabytes[0] = x;
-  return value;
+  Value *valuePtr = makeValue(ui64Value, 1, NULL);
+  make_ui64ValueAt(valuePtr, x);
+  return valuePtr;
 }
 
 void freeValue(Value *value) {
@@ -103,53 +111,90 @@ Expr *makeExpr(ExprType etype, size_t subexpr_count, void *subexprs) {
   return exprPtr;
 }
 
+void freeSubexpr(Expr *exprPtr) {
+  switch (exprPtr->etype) {
+  case ValueExpr: 
+    freeValue(exprPtr->subexprs);
+    break;
+  case BinAddExpr:
+    assert(0 && "TODO");
+  }
+}
+
 /*
  * recursively free expression, subexprs, and values in subexprs.
  * works now because I'm never sharing resources across expressions.
  * will break things later if I start sharing exprs...
  */
 void freeExpr(Expr *exprPtr) {
-  printf("free exprtype %d\n", exprPtr->etype);
-  
   switch (exprPtr->etype) {
   case ValueExpr:
+    printf("freeing value expression\n");
     freeValue(exprPtr->subexprs);
     break;
-  default:
+  case BinAddExpr:
+    printf("freeing bin add expression\n");
     for (int i = 0; i < exprPtr->subexpr_count; ++i) {
-      freeExpr((Expr *)exprPtr->subexprs + i); 
+      printf("freeing bin add subexpr %d\n", i);
+      freeSubexpr((Expr *)(exprPtr->subexprs) + i);
     }
+    printf("freeing subexpression dynamic array");
+    free(exprPtr->subexprs);
     break;
+  default:
+    printf("ERROR: tried to free unknown Expr type\n");
+    exit(1);
   }
   
+  printf("freeing generic expression pointer\n");
   free(exprPtr);
 }
+
+void freeValueExpr(Expr *exprPtr) {
+  freeValue(exprPtr->subexprs);
+  free(exprPtr);
+}
+
+void breaker() { }
 
 int main(int argc, char **argv) {
   
   Value *value1 = make_ui64Value(12345);
   Expr *expr1 = makeExpr(ValueExpr, 1, value1);
 
+  // Expr *expr2_subexprs = malloc(2 * sizeof (Expr));
+  // makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
+  // makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
+  // Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
+
+  // Value *expr2_subexprs = malloc(2 * sizeof (Value));
+  // makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
+  // makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
+  // Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
+
   Expr *expr2_subexprs = malloc(2 * sizeof (Expr));
   makeExprAt(expr2_subexprs + 0, ValueExpr, 1, make_ui64Value(500));
   makeExprAt(expr2_subexprs + 1, ValueExpr, 1, make_ui64Value(40));
   Expr *expr2 = makeExpr(BinAddExpr, 2, expr2_subexprs);
 
-  printf("value1->octabytes[0] == %d\n", value1->octabytes[0]);
-  printf("((Value *)expr1->subexprs)->octabytes[0] == %d\n", ((Value *)expr1->subexprs)->octabytes[0]);
+  breaker();
 
-  printf("((Value *)expr2_subexprs->subexprs)->octabytes[0] == %d\n", ((Value *)expr2_subexprs->subexprs)->octabytes[0]);
-  printf("((Value *)expr2_subexprs->subexprs)->octabytes[0] == %d\n", ((Value *)(expr2_subexprs + 1)->subexprs)->octabytes[0]);
 
-  printf("%d\n",
-	 ((Value *)(expr2_subexprs + 0)->subexprs)->octabytes[0]
-	 +
-	 ((Value *)(expr2_subexprs + 1)->subexprs)->octabytes[0]);
+  // printf("value1->octabytes[0] == %d\n", value1->octabytes[0]);
+  // printf("((Value *)expr1->subexprs)->octabytes[0] == %d\n", ((Value *)expr1->subexprs)->octabytes[0]);
+
+  // printf("((Value *)expr2_subexprs->subexprs)->octabytes[0] == %d\n", ((Value *)expr2_subexprs->subexprs)->octabytes[0]);
+  // printf("((Value *)expr2_subexprs->subexprs)->octabytes[0] == %d\n", ((Value *)(expr2_subexprs + 1)->subexprs)->octabytes[0]);
+
+  // printf("%d\n",
+	//  ((Value *)(expr2_subexprs + 0)->subexprs)->octabytes[0]
+	//  +
+	//  ((Value *)(expr2_subexprs + 1)->subexprs)->octabytes[0]);
 
 
  
   
-  /* freeValue(value1); */
+  // /* freeValue(value1); */
 
 
   printf("free expr1\n");
